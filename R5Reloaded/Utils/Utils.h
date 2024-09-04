bdd7a478b8e9cfe4c8171751942546d8dddd635f
Neko64V <sawayump@gmail.com>
2024-09-05 04:18:17 +0900

    Delete R5Reloaded directory

--- R5Reloaded/Utils/Utils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-#include <string>
-#include "Config/Config.h"
-
-extern bool IsKeyDown(int VK);
-extern void KeyBinder(int& target_key, int& BindID);
-extern const char* KeyNames[];
