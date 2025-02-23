90523f3c8572dca6c5c9e894a3161508eba698c2
Neko64V <sawayump@gmail.com>
2025-02-23 12:27:57 +0900

    MemoryクラスをCS:2 Externalと共通化

--- CFramework/Framework/Memory/Memory.cpp
+++ CFramework/Framework/Memory/Memory.cpp
@@ -49,7 +49,7 @@ bool Memory::AttachProcess(const char* targetName, int InitMode)
 void Memory::GetBaseAddress(const char* targetName)
 {
     // ベースアドレスを取得
-    m_gProcessBaseAddr = GetModuleBase(targetName);
+    m_gProcessBaseAddr = GetModuleBaseAddress(targetName);
 
     if (m_gProcessBaseAddr == 0)
         MessageBoxA(nullptr, "BaseAddress == 0", "WARNING", MB_OK | MB_TOPMOST);
@@ -60,7 +60,68 @@ void Memory::DetachProcess()
     CloseHandle(m_hProcess);
 }
 
-uintptr_t Memory::GetModuleBase(const std::string moduleName)
+uintptr_t Memory::FindPattern(const std::vector<uint8_t>& read_data, uintptr_t base_address, const std::string pattern, int offset, int extra)
+{
+    std::vector<uint8_t> bytes;
+    std::istringstream patternStream(pattern);
+    std::string byteStr;
+
+    while (patternStream >> byteStr) {
+        if (byteStr == "?" || byteStr == "??")
+            bytes.push_back(0);
+        else
+            bytes.push_back(static_cast<uint8_t>(strtol(byteStr.c_str(), nullptr, 16)));
+    }
+
+    for (size_t i = 1000000; i < read_data.size(); ++i)
+    {
+        bool patternMatch = true;
+        for (size_t j = 0; j < bytes.size(); ++j)
+        {
+            if (bytes[j] != 0 && read_data[i + j] != bytes[j])
+            {
+                patternMatch = false;
+                break;
+            }
+        }
+
+        if (patternMatch) {
+            uintptr_t patternAddress = base_address + i;
+            int32_t of;
+            ReadProcessMemory(m_hProcess, reinterpret_cast<LPCVOID>(patternAddress + offset), &of, sizeof(of), nullptr);
+            uintptr_t result = patternAddress + of + extra;
+            bytes.clear();
+
+            return (result - base_address);
+        }
+    }
+}
+
+MODULEINFO Memory::GetModuleInfo(const std::string moduleName)
+{
+    DWORD cb;
+    HMODULE hMods[256]{};
+    MODULEINFO modInfo{};
+
+    if (EnumProcessModules(m_hProcess, hMods, sizeof(hMods), &cb))
+    {
+        for (unsigned int i = 0; i < (cb / sizeof(HMODULE)); i++)
+        {
+            char szModName[MAX_PATH];
+            if (GetModuleBaseNameA(m_hProcess, hMods[i], szModName, sizeof(szModName) / sizeof(char)))
+            {
+                if (moduleName == szModName) {
+                    GetModuleInformation(m_hProcess, hMods[i], &modInfo, sizeof(modInfo));
+                    break;
+                }
+            }
+        }
+    }
+
+    return modInfo;
+}
+
+uintptr_t Memory::GetModuleBaseAddress(const std::string moduleName)
 {
     MODULEENTRY32 entry{};
     entry.dwSize = sizeof(MODULEENTRY32);
@@ -80,6 +141,28 @@ uintptr_t Memory::GetModuleBase(const std::string moduleName)
 
     return reinterpret_cast<uintptr_t>(entry.modBaseAddr);
 }
+
+MODULEENTRY32 Memory::GetModule(const std::string moduleName)
+{
+    MODULEENTRY32 entry{};
+    entry.dwSize = sizeof(MODULEENTRY32);
+    const auto snapShot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, m_dwPID);
+
+    while (Module32Next(snapShot, &entry))
+    {
+        if (!moduleName.compare(entry.szModule))
+        {
+            CloseHandle(snapShot);
+            return entry;
+        }
+    }
+
+    if (snapShot)
+        CloseHandle(snapShot);
+
+    return entry;
+}
+
 
 PROCESSENTRY32 Memory::GetProcess(const std::string processName)
 {
--- CFramework/Framework/Memory/Memory.h
+++ CFramework/Framework/Memory/Memory.h
@@ -2,8 +2,10 @@
 #include <iostream>
 #include <Windows.h>
 #include <TlHelp32.h>
-#include <psapi.h>
 #include <string>
+#include <vector>
+#include <Psapi.h>
+#include <sstream>
 
 /*	[+] メモリやオーバーレイの初期化モードを設定します
 
@@ -23,19 +25,20 @@ class Memory
 private:
 	DWORD m_dwPID;
 	HANDLE m_hProcess;
-	const char* m_CTargetModule = "r5apex.exe"; // ベースアドレスを取得したいモジュール名
-
-	uintptr_t GetModuleBase(const std::string moduleName);
-	PROCESSENTRY32 GetProcess(const std::string processName);
 public:
 	uintptr_t m_gProcessBaseAddr;
-	// uintptr_t m_gClientDllBaseAddr;
-	// uintptr_t m_gEngineDllBaseAddr;
 
 	bool AttachProcess(const char* targetName, int InitMode);
 	void GetBaseAddress(const char* targetName);
 	void DetachProcess();
 
+	uintptr_t GetModuleBaseAddress(const std::string moduleName);
+	MODULEINFO GetModuleInfo(const std::string moduleName);
+	MODULEENTRY32 GetModule(const std::string moduleName);
+	PROCESSENTRY32 GetProcess(const std::string processName);
+	uintptr_t FindPattern(const std::vector<uint8_t>& read_data, uintptr_t base_address, const std::string pattern, int offset, int extra);
+
+	// R/WPM Template
 	template <typename T>
 	constexpr const T Read(const uintptr_t& address) const noexcept
 	{
@@ -48,10 +51,30 @@ public:
 	{
 		WriteProcessMemory(m_hProcess, reinterpret_cast<void*>(address), &value, sizeof(T), NULL);
 	}
-	bool ReadString(uintptr_t address, LPVOID buffer, SIZE_T size) const
+	bool ReadString(const uintptr_t address, LPVOID buffer, SIZE_T size) const noexcept
 	{
-		SIZE_T size_read;
-		return !!::ReadProcessMemory(m_hProcess, LPCVOID(address), buffer, size, &size_read) && size_read > 0;
+		return !!::ReadProcessMemory(m_hProcess, (void*)(address), buffer, size, nullptr);
+	}
+	std::string ReadStringA(const uintptr_t address) const noexcept
+	{
+		char vOut[MAX_PATH]{};
+		ReadProcessMemory(m_hProcess, (void*)(address), vOut, sizeof(vOut), nullptr);
+
+		return std::string(vOut);
+	}
+	std::vector<uint8_t> ReadBytes(uintptr_t address, size_t size)
+	{
+		std::vector<uint8_t> buffer(size);
+		ReadProcessMemory(m_hProcess, reinterpret_cast<LPCVOID>(address), buffer.data(), size, nullptr);
+		return buffer;
+	}
+	uintptr_t ReadChain(uintptr_t address, const std::vector<uintptr_t>& offsets)
+	{
+		uintptr_t result = Read<uintptr_t>(address + offsets.at(0));
+		for (int i = 1; i < offsets.size(); i++)
+			result = Read<uintptr_t>(result + offsets.at(i));
+
+		return result;
 	}
 };
 
