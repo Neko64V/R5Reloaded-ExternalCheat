0e39d135184809a1bb5923c2fe0eb94b7c6de776
Neko64V <sawayump@gmail.com>
2025-02-23 10:10:55 +0900

    フォントを追加

--- CFramework/Framework/ImGui/Custom.cpp
+++ CFramework/Framework/ImGui/Custom.cpp
@@ -1,7 +1,23 @@
 #include "Custom.h"
 
-// Button
-bool ImGui::CustomButton(const char* label, const ImVec2& size_arg, const bool& flag)
+// ToDo : 全体的に書き直す
+
+void ImGui::RenderCustomFrame(ImVec2 p_min, ImVec2 p_max, ImU32 fill_col, ImU32 sub_col, bool border, float rounding)
+{
+    ImGuiContext& g = *GImGui;
+    ImGuiWindow* window = g.CurrentWindow;
+    window->DrawList->AddRectFilled(p_min, p_max, fill_col, rounding);
+    window->DrawList->AddRectFilled(ImVec2(p_min.x, p_min.y), ImVec2(p_min.x + 3.f, p_max.y), sub_col, 0.f);
+
+    const float border_size = g.Style.FrameBorderSize;
+    if (border && border_size > 0.0f)
+    {
+        window->DrawList->AddRect(ImVec2(p_min.x + 1.f, p_min.y + 1.f), ImVec2(p_max.x + 1.f, p_max.y + 1.f), GetColorU32(ImGuiCol_BorderShadow), rounding, 0, border_size);
+        window->DrawList->AddRect(p_min, p_max, GetColorU32(ImGuiCol_Border), rounding, 0, border_size);
+    }
+}
+
+bool ImGui::CustomButton(const char* icon, const char* label, const ImVec2& size_arg, const bool& flag)
 {
     ImGuiWindow* window = GetCurrentWindow();
     if (window->SkipItems)
@@ -9,11 +25,11 @@ bool ImGui::CustomButton(const char* label, const ImVec2& size_arg, const bool& 
 
     ImGuiContext& g = *GImGui;
     const ImGuiStyle& style = g.Style;
-    const ImGuiID id = window->GetID(label);
-    const ImVec2 label_size = CalcTextSize(label, NULL, true);
+    const ImGuiID id = window->GetID(icon);
+    const ImVec2 label_size = CalcTextSize(icon, NULL, true);
 
     ImVec2 pos = window->DC.CursorPos;
-    ImVec2 size = CalcItemSize(size_arg, label_size.x + style.FramePadding.x * 2.0f, label_size.y + style.FramePadding.y * 2.0f);
+    ImVec2 size = CalcItemSize(size_arg, label_size.x + style.FramePadding.x, label_size.y + style.FramePadding.y);
 
     const ImRect bb(pos, ImVec2(pos.x + size.x, pos.y + size.y));
     ItemSize(size, style.FramePadding.y);
@@ -31,8 +47,17 @@ bool ImGui::CustomButton(const char* label, const ImVec2& size_arg, const bool& 
 
     if (g.LogEnabled)
         LogSetNextTextDecoration("[", "]");
-    RenderTextClipped(ImVec2(bb.Min.x + style.FramePadding.x, bb.Min.y + style.FramePadding.y), ImVec2(bb.Max.x - style.FramePadding.x, bb.Max.y - style.FramePadding.y), label, NULL, &label_size, style.ButtonTextAlign, &bb);
 
+    /*
+    icon の label_size.x が均一とは限らない（し、めんどくさい）のでマジックナンバーを入れるしかない。
+    要修正
+    */
+
+    // Icon
+    RenderTextClipped(ImVec2(bb.Min.x + style.FramePadding.x + 10, bb.Min.y), ImVec2(bb.Min.x - style.FramePadding.x - 10, bb.Max.y), icon, NULL, &label_size, style.ButtonTextAlign, &bb);
+    // Text
+    RenderTextClipped(ImVec2(bb.Min.x - 20, bb.Min.y), ImVec2(bb.Max.x - 20, bb.Max.y), label, NULL, &label_size, style.ButtonTextAlign, &bb);
+    
     // Automatically close popups
     //if (pressed && !(flags & ImGuiButtonFlags_DontClosePopups) && (window->Flags & ImGuiWindowFlags_Popup))
     //    CloseCurrentPopup();
@@ -41,6 +66,7 @@ bool ImGui::CustomButton(const char* label, const ImVec2& size_arg, const bool& 
     return pressed;
 }
 
+/*
 // RenderFrame
 void ImGui::RenderCustomFrame(ImVec2 p_min, ImVec2 p_max, ImU32 fill_col, ImU32 sub_col, bool border, float rounding)
 {
@@ -56,3 +82,49 @@ void ImGui::RenderCustomFrame(ImVec2 p_min, ImVec2 p_max, ImU32 fill_col, ImU32 
         window->DrawList->AddRect(p_min, p_max, GetColorU32(ImGuiCol_Border), rounding, 0, border_size);
     }
 }
+
+// Button
+bool ImGui::CustomButton(const char* label, const ImVec2& size_arg, const bool& flag)
+{
+    ImGuiWindow* window = GetCurrentWindow();
+    if (window->SkipItems)
+        return false;
+
+    ImGuiContext& g = *GImGui;
+    const ImGuiStyle& style = g.Style;
+    const ImGuiID id = window->GetID(label);
+    const ImVec2 label_size = CalcTextSize(label, NULL, true);
+
+    ImVec2 pos = window->DC.CursorPos;
+    ImVec2 size = CalcItemSize(size_arg, label_size.x + style.FramePadding.x, label_size.y + style.FramePadding.y);
+
+    const ImRect bb(pos, ImVec2(pos.x + size.x, pos.y + size.y));
+    ItemSize(size, style.FramePadding.y);
+    if (!ItemAdd(bb, id))
+        return false;
+
+    bool hovered, held;
+    bool pressed = ButtonBehavior(bb, id, &hovered, &held, ImGuiButtonFlags_None);
+
+    // Render
+    const ImU32 col = GetColorU32((held && hovered) ? ImGuiCol_ButtonActive : hovered ? ImGuiCol_ButtonHovered : ImGuiCol_Button);
+    const ImU32 sub = GetColorU32(flag ? ImGuiCol_CheckMark : ImGuiCol_ButtonActive);
+    RenderNavHighlight(bb, id);
+    RenderCustomFrame(bb.Min, bb.Max, col, sub, true, style.FrameRounding);
+
+    if (g.LogEnabled)
+        LogSetNextTextDecoration("[", "]");
+
+    RenderTextClipped(ImVec2(bb.Min.x + style.FramePadding.x + 10, bb.Min.y + style.FramePadding.y), ImVec2(bb.Min.x - style.FramePadding.x - 10, bb.Max.y - style.FramePadding.y), label, NULL, &label_size, style.ButtonTextAlign, &bb);
+
+    // Text
+    //RenderTextClipped(ImVec2(bb.Min.x + style.FramePadding.x, bb.Min.y + style.FramePadding.y), ImVec2(bb.Min.x - style.FramePadding.x - 10, bb.Max.y - style.FramePadding.y), label, NULL, &label_size, style.ButtonTextAlign, &bb);
+
+    // Automatically close popups
+    //if (pressed && !(flags & ImGuiButtonFlags_DontClosePopups) && (window->Flags & ImGuiWindowFlags_Popup))
+    //    CloseCurrentPopup();
+
+    IMGUI_TEST_ENGINE_ITEM_INFO(id, label, g.LastItemData.StatusFlags);
+    return pressed;
+}
+*/
--- CFramework/Framework/ImGui/Custom.h
+++ CFramework/Framework/ImGui/Custom.h
@@ -7,5 +7,5 @@ namespace ImGui
     void RenderCustomFrame(ImVec2 p_min, ImVec2 p_max, ImU32 fill_col, ImU32 sub_col, bool border, float rounding);
 
     // Button
-    bool CustomButton(const char* label, const ImVec2& size_arg, const bool& flag);
+    bool CustomButton(const char* icon, const char* label, const ImVec2& size_arg, const bool& flag);
 }
